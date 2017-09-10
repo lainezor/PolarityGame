2190f23cd1af38b869ef53748cf6dd28d109f472
lainezor <lainezor@live.ca>
2017-09-10 14:48:19 -0400

    first commit
    really basic 2D platformer stuff

---
+++ 2D Platformer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed, jumpHeight;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rigidbody2D.velocity = new Vector2(0, jumpHeight);
+				}
+		if (Input.GetKey(KeyCode.D)) {
+			rigidbody2D.velocity = new Vector2 (moveSpeed,rigidbody2D.velocity.y);
+				}
+		if (Input.GetKey(KeyCode.A)) {
+			rigidbody2D.velocity = new Vector2(-moveSpeed, rigidbody2D.velocity.x);
+		}
+
+	}
+}
